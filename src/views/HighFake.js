96323889c0a36f83f999486ac9e0fc997355d77d
Rafal Labisz <rafal0710@gmail.com>
2019-06-01 01:49:22 +0200

    nav

---
+++ src/views/HighFake.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 30px 30px 30px 180px;
+`;
+const HighFake = () => {
+  return (
+    <Wrapper>
+      <div>Card High</div>
+    </Wrapper>
+  );
+};
+
+export default HighFake;
---
+++ src/views/LowFake.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 30px 30px 30px 180px;
+`;
+const LowFake = () => {
+  return (
+    <Wrapper>
+      <div>Card Low</div>
+    </Wrapper>
+  );
+};
+
+export default LowFake;
