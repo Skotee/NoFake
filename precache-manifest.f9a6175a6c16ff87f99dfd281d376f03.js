64fb437acb5e56121825b534086e3e45bd892bb4
Radek <radexw1@gmail.com>
2019-06-03 00:12:13 +0200

    Updates

---
+++ precache-manifest.f9a6175a6c16ff87f99dfd281d376f03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55339c4c259a64be34e046edadb5ecf3",
+    "url": "/Skotee/NoFake/index.html"
+  },
+  {
+    "revision": "faddff5d59df97aa03fe",
+    "url": "/Skotee/NoFake/static/js/2.31de7c87.chunk.js"
+  },
+  {
+    "revision": "13b040712d12d1d288ba",
+    "url": "/Skotee/NoFake/static/js/main.7294d4e8.chunk.js"
+  },
+  {
+    "revision": "82619e2d87a19b6612ac",
+    "url": "/Skotee/NoFake/static/js/runtime~main.ea4cf0b5.js"
+  }
+]);
